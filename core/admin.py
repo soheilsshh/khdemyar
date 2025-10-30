a0e5641708e4d455eef7a52d0b6edabb1afdb03b
soheilsshh <soheilswhh@gmail.com>
2025-10-30 12:24:19 +0330

    make admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User, Employee, Shift, BlogPost
+
+admin.site.register(User)
+admin.site.register(Employee)
+admin.site.register(Shift)
+admin.site.register(BlogPost)
