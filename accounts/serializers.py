4ea044ea3337e74151e0dceb9116726676f2fb21
soheilsshh <soheilswhh@gmail.com>
2025-11-08 09:43:47 +0330

    add Employee serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,20 +1,41 @@
+# account/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import authenticate
-from core.models import User
+from core.models import User, Employee
 
+class EmployeeRegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+    phone_number = serializers.CharField(write_only=True)
 
-class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'first_name', 'last_name']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        user.is_active = False
-        user.save()
-        return user
-
+        model = Employee
+        fields = [
+            'username',
+            'password',
+            'phone_number',
+            'first_name',
+            'last_name',
+            'national_id',
+            'father_name',
+            'birth_date',
+            'gender',
+            'marital_status',
+            'children_count',
+            'education_level',
+            'field_of_study',
+            'nationality',
+            'religion', 
+            'sect', 
+            'has_criminal_record',
+            'phone', 
+            'social_phone', 
+            'work_phone', 
+            'home_phone',
+            'work_address', 
+            'home_address', 
+            'military_status'
+        ]
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -22,13 +43,21 @@ class LoginSerializer(serializers.Serializer):
 
     def validate(self, data):
         user = authenticate(username=data['username'], password=data['password'])
-        if user is None:
+        if not user:
             raise serializers.ValidationError("نام کاربری یا رمز عبور اشتباه است.")
+        if not user.is_active:
+            raise serializers.ValidationError("حساب شما توسط مدیر تأیید نشده است.")
         data['user'] = user
         return data
 
+class OTPRequestSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(max_length=20)
+
+class OTPVerifySerializer(serializers.Serializer):
+    phone_number = serializers.CharField(max_length=20)
+    code = serializers.CharField(max_length=6)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_approved']
