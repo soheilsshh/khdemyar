539cd84056a213755faaf0548131262681c73387
soheilsshh <soheilswhh@gmail.com>
2025-11-09 10:55:01 +0330

    add swagger to projetc

--- khademyar/urls.py
+++ khademyar/urls.py
@@ -16,9 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Khademyar API",
+      default_version='v1',
+      description="API documentation for Khademyar project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="soheilswhh@gmail.com"),
+      license=openapi.License(name="MIT"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/core/', include('core.urls')),
+    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
